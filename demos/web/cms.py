772650406d47244c4294464ec986a9402db19544
ryan <ryan@461db578-a313-0410-9c92-fd5386c7d758>
2006-07-16 18:31:22 +0000

    Oops. Here's what should've been committed for rev:34

--- demos/web/cms.py
+++ demos/web/cms.py
@@ -2,36 +2,158 @@
 
 import cells, BaseHTTPServer, ConfigParser, glob, os, re, time, datetime
 
-# BIG props to Yuri Takhteyev for markdown.py. 
-import markdown
-
 CellCMSConfig = ConfigParser.ConfigParser()
 CellCMSConfig.read('cms.cfg')
 
-# the general idea for this thing is that there be a thin shell of
-# "normal" python to set up & feed the models which will do the heavy
-# lifting & responses.
+class PageCache(object):
+    """Builds & stores Page models"""
+    def __init__(self):
+        self.cache = {}
+        
+    def __getitem__(self, request):
+        # we don't actually want to use the whole request object as the
+        # key, just the bits we care about:
+        page = self.cache.get(request.path, None)
+        
+        if not page:           # and if it doesn't exist in the cache, build it
+            print "Building page for", request.path
+            # XXX: eventually, we'd chose a template based on the request
+            page = Page(cache=self, request=request)
+            self.cache[request.path] = page # and put it in the cache
+        else:                  # if it does exist, make sure its source is curr
+            print "Page cache updating mod-time on", request.path
+            if page.is_static:
+                page.modified = os.stat(page.static_path)[9]
+            elif page.is_dynamic:
+                page.modified = os.stat(page.dynamic_path)[9]
 
-# let's build the models.
-class Template(cells.Model):
-    """A model which takes some HTML and adds a template around it"""
+        print "Cache has", repr(self.cache.keys())
+
+        return page.output
+
+    
+class Page(cells.Model):
+    def __init__(self, cache, *args, **kwargs):
+        self.cache = cache
+        self.config = CellCMSConfig
+        cells.Model.__init__(self, *args, **kwargs)
+
+    special = re.compile(r"/__.*__")
+    cleanpath = re.compile(r"(/([A-Za-z0-9]+\.)*([A-Za-z0-9]*))*(\?.*)?")
+    
+    # INPUT CELLS
+    request = cells.makecell(value=None)
+    modified = cells.makecell(value=0)
+    built = cells.makecell(value=0)
+
+    # RULE CELLS
+    @cells.fun2cell()
+    def dirty_path(self, prev):
+        if not self.request:
+            return "/"
+        return self.request.path
+    
+    @cells.fun2cell()
+    def cleaned_path(self, prev):
+        if not self.cleanpath.match(self.dirty_path):
+            print "Illegal path request:", self.dirty_path
+            return "/"
+        return self.dirty_path
+
+    @cells.fun2cell()
+    def dynamic_path(self, prev):
+        return (self.config.get('directories', 'storage') + self.cleaned_path)
+
+    @cells.fun2cell()
+    def static_path(self, prev):
+        return (self.config.get('directories', 'static') + self.cleaned_path)
+    
+    is_dynamic = cells.makecell(rule=lambda s,p: glob.glob(s.dynamic_path))
+    is_static = cells.makecell(rule=lambda s,p: glob.glob(s.static_path))
+
+    @cells.fun2cell()
+    def is_directory(self, prev):
+        return os.path.isdir(self.static_path)
+
+    @cells.fun2cell()
+    def is_special(self, prev):
+        return self.special.match(self.cleaned_path)
+
+    @cells.fun2cell()
+    def should_apply_template(self, prev):
+        return self.is_dynamic and not self.is_special
+
+    @cells.fun2cell()
+    def is_valid(self, prev):
+        return self.is_dynamic or self.is_static
+
+    @cells.fun2cell()
+    def source(self, prev):
+        """Holds the unrendered source data for this Page"""
+        if not self.is_valid:
+            return "No such file"
+
+        # all we need to do is depend on self.modified and the source
+        # will be updated whenever the modification-time is
+        # changed. I'm pretty sure this test will always fail.
+        if self.modified < self.built:
+            return prev
+        
+        if self.is_directory:           # implies is_static
+            currdir = os.getcwd()       # save old dir
+            os.chdir(self.static_path)  # change to the requested dir
+            out = ["Directory listing:", "------------------------------"]
+            out += glob.glob("*")       # get the files here
+            os.chdir(currdir)           # change back to the original dir
+            return "\n".join(out)
+
+        if self.is_static:              # because of above test, not a dir.
+            return open(self.static_path).read()
+        
+        if self.is_dynamic:
+            return open(self.dynamic_path).read()
+
+        else:
+            return "Should not have got here!"
+    
+    @cells.fun2cell()
+    def template(self, prev):
+        """Applies a template to this Page depending on request type"""
+        # (doesn't actually make any choices, currently)
+        if self.should_apply_template:
+            return Template(cache=self.cache, text=self.source)
+        
+    @cells.fun2cell()
+    def output(self, prev):
+        if self.should_apply_template:
+            return self.template.render()
+        else:
+            return self.source
+
+@Page.observer(attrib="source")
+def source_obs(self):
+    """Each time a new source gets generated, update its built-time
+    """
+    if self.is_valid:
+        self.built = self.modified
+
+@Page.observer(attrib="output")
+def out_obs(self):
+    print self.cleaned_path, "-- New output"
+
+class Template(object):
     # this could probably be done with one regex. I'm too lazy to figure it out.
     includetag1 = re.compile(r"\<\s*include_resource\s*name\s*=\s*" +\
                              r"'(?P<resource>[^']+)'\s*\/\>")
     includetag2 = re.compile(r'\<\s*include_resource\s*name\s*=\s*' +\
                              r'"(?P<resource>[^"]+)"\s*\/\>')
-    
-    def __init__(self, pagecache, *args, **kwargs):
-        self.pagecache = pagecache
-        cells.Model.__init__(self, *args, **kwargs)
-    
-    # INPUT CELLS
-    source = cells.makecell(value="")
-    template = cells.makecell(value="default.tmpl")
 
-    # RULE CELLS
-    @cells.fun2cell()
-    def output(self, prev):
+    def __init__(self, text, cache, template="default.tmpl"):
+        self.source = text
+        self.cache = cache
+        self.template = template
+
+    def render(self):
         print "Running template.output with", repr(self.source[:10])
         rendered = ""
         
@@ -51,7 +173,8 @@ class Template(cells.Model):
                 # otherwise, just pull from the page cache (which may build
                 # another Page)
                 else:
-                    replacement = self.pagecache[m.group(1)]
+                    req = Request(path="/" + m.group(1)) # dummy request
+                    replacement = self.cache[req]
 
                 line = line[:m.span()[0]] + replacement + line[m.span()[1]:]
                             
@@ -61,243 +184,38 @@ class Template(cells.Model):
                     m = self.includetag2.search(line)
 
             rendered += line
-
+            
         return rendered
 
-    
-class Page(cells.Model):
-    """A model to hold a single page, from request to page generation."""
-    # Non-Cells
-    cleanpath = re.compile(r"(/([A-Za-z0-9]+\.)*([A-Za-z0-9]*))*(\?.*)?")
-    def __init__(self, pagecache, *args, **kwargs):
-        self.pagecache = pagecache
-        cells.Model.__init__(self, *args, **kwargs)
-    
-    # INPUT CELLS
-    request = cells.makecell(value=None) # the Request object which builds this
-    config = cells.makecell(value=None)  # a ConfigParser object
-    built = cells.makecell(value=0)      # when this Page was last (re)built
-    
-    @cells.fun2cell(type=cells.RuleThenInputCell)
-    def template(self, prev):
-        t = Template(pagecache=self.pagecache)
-        return t
-    
-    @cells.fun2cell(type=cells.RuleThenInputCell)
-    def modified(self, prev):
-        """When the data this cell depends on last changed"""
-        path = self.source_info['path']
-        if path:
-            return os.stat(self.source_info['path'])[9]
-        else:
-            return 0.0
+# Make a page cache object
+pages = PageCache()
 
-    # RULE CELLS
-    @cells.fun2cell()
-    def cleaned_path(self, prev):
-        if not self.cleanpath.match(self.request.path):
-            print "Illegal path request:", self.request.path
-            return "/"
-        return self.request.path
-
-    @cells.fun2cell()
-    def source_info(self, prev):
-        """Returns some metadata about the source file for this Page"""
-        response = { 'static': True,
-                     'directory': True,
-                     'path': None,
-                     'valid': True }
-        if glob.glob(self.config.get('directories', 'static') +
-                     self.cleaned_path):
-            staticpath = self.config.get('directories', 'static') + \
-                         self.cleaned_path
-            response['path'] = staticpath
-            if os.path.isdir(staticpath):
-                return response
-            else:
-                response['directory'] = False
-                return response
-        if glob.glob(self.config.get('directories', 'storage') + 
-                     self.cleaned_path):
-            fullpath = self.config.get('directories', 'storage') + \
-                       self.cleaned_path
-            response['static'] = False
-            response['directory'] = False
-            response['path'] = fullpath
-            return response
-        else:                           # error!
-            response['valid'] = False
-            return response
-
-    @cells.fun2cell()
-    def source(self, prev):
-        """Holds the unrendered source data for this Page"""
-        if not self.source_info['valid']:
-            return "No such file"
-
-        # all we need to do is depend on self.modified and the source
-        # will be updated whenever the modification-time is
-        # changed. I'm pretty sure this test will always fail.
-        if self.modified <= self.built:
-            return prev
-        
-        if self.source_info['static'] and self.source_info['directory']:
-            currdir = os.getcwd()     # save old dir
-            os.chdir(self.source_info['path']) # change to the requested dir
-            out = ["Directory listing:", "------------------------------"]
-            out += glob.glob("*")   # get the files here
-            os.chdir(currdir)       # change back to the original dir
-            return "\n".join(out)
-
-        if self.source_info['static'] and not self.source_info['directory']:
-            return open(self.source_info['path']).read()
-        
-        if not self.source_info['static']:
-            return open(self.source_info['path']).read()
-
-        else:
-            return "Weird..."
-
-    @cells.fun2cell()
-    def templatized(self, prev):
-        """Runs the source through a templating system"""
-        # we only want to templatize dynamic content:
-        if self.source_info['static'] or not self.source_info['path']: return ""
-        # get the templatized page back
-        return self.template.output
-        
-    @cells.fun2cell()
-    def output(self, prev):        
-        """Outputs the final, rendered version of this Page"""
-        print "Page.output running on", repr(self.source_info['path'])
-        out = ""
-        if self.source_info['static']:
-            out += self.source            
-        else:
-            out += self.templatized
-            time = datetime.datetime.fromtimestamp(self.modified).ctime()
-            out += "\nResource last changed:" + time
-        
-        return out
-        
-
-@Page.observer(attrib="request")
-def pagelog_observer(self):
-    """Each time a Page is created or modified, log."""
-    if self.request:
-        print self.request.client_address[0] + ":", str(self.request.path), \
-              "-- created Page"
-
-@Page.observer(attrib="source")
-def source_obs(self):
-    """Each time a new source gets generated, update its built
-    time and feed it to the Page's template
-    """
-    if self.source_info['path']:
-        self.built = self.modified
-
-        if not self.source_info['static']:
-            # first, turn the source into html by running it through markdown
-            print "Templatizing", self.source_info['path']
-            rendered_source = markdown.markdown(self.source)
-            # and now push the rendered source into the template
-            self.template.source = rendered_source
-
-
-class PageCache(cells.Model):
-    """Holds the Page objects; builds them upon requests"""
-    request_handler = None
-    
-    def __init__(self):
-        self.cache = {}
-        
-    def __getitem__(self, key):
-        page = self.cache.get(key, None)
-        if page:                        # exists? update the page
-            print "Page cache updating mod-time on", repr(key)
-            page.modified = os.stat(page.source_info['path'])[9]
-        else:                           # doesn't? make the page
-            page = Page(config=self.request_handler.config,
-                        request=self.request_handler.request,
-                        pagecache=self)
-            page.pagecache = self
-            self.cache[key] = page
-
-        return page
-
-    def __setitem__(self, key, value):
-        print "Adding page for", repr(key), "to cache (cache now has", \
-              str(len(self.cache.keys())), "keys)"
-        self.cache[key] = value
-
-    
-class RequestHandler(cells.Model):
-    """A model which turns GET requests into formatted content"""
-    # NONCELLS
-    special = ('/stats',)
-    cache = None
-        
-    # INPUT CELLS
-    config = cells.makecell(value=None)  # handle to the ConfigParser object    
-    request = cells.makecell(value=None) # the input for the model
-
-    # RULE CELLS        
-    @cells.fun2cell()
-    def request_key(self, prev):
-        """Takes a BaseHTTPRequestHandler and makes a page key out of it"""
-        if self.request:
-            req = self.request
-            return req.path        # eventually, more would go in here
-        return None
-
-    @cells.fun2cell()
-    def output(self, prev):
-        """Outputs the requested page, rendered and ready for the browser."""
-        if not self.request_key: return
-        
-        if self.request.path == '/stats':
-            buff = ["Cell CMS Statistics",
-                    "Number of stored Pages: " + str(len(self.pages.keys())),
-                    str([str(key) for key in self.pages.keys()])]
-            return "\n".join(buff)
-            
-        return self.cache[self.request_key]
-
-@RequestHandler.observer(attrib="request")
-def handlerlog_observer(self):
-    """Each time a request is recieved, log"""
-    if self.request:
-        print self.request.client_address[0] + ":", str(self.request.path), \
-              "-- got request"
-        
-    
-handler = RequestHandler()             # make a new cellular handler
-handler.config = CellCMSConfig         # and give it its configuration
-
-pcache = PageCache()                   # make a new page cache
-pcache.request_handler = handler
-handler.cache = pcache                 # and give it to the RequestHandler
-    
-# now let's build the shell. I'm going to use the BaseHTTPServer to
-# process the raw requests, and a subclass of the base request handler
-# will pass things into the model
 class Request(object):
     """Just a container for the RequestHandler's current request data"""
-    def __init__(self, request):
-        self.client_address = request.client_address
-        self.command = request.command
-        self.path = request.path
-        self.request_version = request.request_version
-        self.headers = request.headers
-        self.rfile = request.rfile
-        self.wfile = request.wfile
+    def __init__(self, request=None, path=None):
+        if request:
+            self.client_address = request.client_address
+            self.command = request.command
+            self.path = request.path
+            self.request_version = request.request_version
+            self.headers = request.headers
+            self.rfile = request.rfile
+            self.wfile = request.wfile
+        else:
+            self.client_address = None
+            self.command = None
+            self.path = path
+            self.request_version = None
+            self.headers = None
+            self.rfile = None
+            self.wfile = None
                                                   
 class CellRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
-        handler.request = Request(self) # build a new request object
-        print >>self.wfile, handler.output
+        print >>self.wfile, pages[Request(request=self)]
 
-# Finally, set it moving:
+    
+# Finally, set the server moving:
 print "Starting server at", CellCMSConfig.get('server', 'address'), "on port", \
       CellCMSConfig.get('server', 'port')
 server_address=(CellCMSConfig.get('server', 'address'),
@@ -305,4 +223,3 @@ server_address=(CellCMSConfig.get('server', 'address'),
 httpd = BaseHTTPServer.HTTPServer(server_address, CellRequestHandler)
 print "... server started."
 httpd.serve_forever()
-
